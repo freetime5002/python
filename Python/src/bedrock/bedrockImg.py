2f070b73c95ff80f5de85cc7b5c99ff958241b12
freetime5002 <freetime5002@gmail.com>
2025-04-04 16:06:25 +0900

    파일 수정

---
+++ Python/src/bedrock/bedrockImg.py
@@ -0,0 +1,39 @@
+import boto3
+from botocore.exceptions import ClientError
+
+#이미지 생성 작업을 위한 베트락 런타임 클라이언트 생성
+client = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+
+image_path = "Draw a student studying AI in a cartoon style.png"
+with open(image_path, "rb") as image_file:
+    image_bytes = image_file.read()
+
+#이미지와 이미지 분석 요청 텍스트가 포함 된 ContentBlock 작성
+user_message = "서문 없이 사진을 분석 한 결과를 작성해 주세요" 
+conversation = {
+    "role": "user",
+    "content": [
+        {"text":user_message},
+        {"image":{"format" : "png",
+                  "source" : {
+                      "bytes": image_bytes
+                    }
+                  }
+         }
+    ]
+}
+
+try:
+    response = client.invoke_model(modelId = model_id, 
+                                    message = conversation,
+                                    inferenceConfig={"maxTokens":4096, "temperature":0.5,"topP":0.9}, 
+                                    )
+    
+    response_text = response["output"]["message"]["content"][0]["text"]
+    print(response_text)
+except(ClientError,Exception) as e:
+    print(f"ERROR : Can't invoke '{model_id}'.Reason:{e}")
+    exit(1)
+    
